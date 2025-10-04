be2df750804b665e81a8c404b91f2f6040d96bee
Soufiane EL KASSIB <elkassib.soufiane@gmail.com>
2025-10-04 17:44:48 +0100

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [],
+    base: '/', // Ensure correct base path for assets
+    output: 'static', // Static site generation
+});
